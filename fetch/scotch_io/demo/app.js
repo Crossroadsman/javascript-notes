992ad2ed795973a407778f7047aca31a4ff7eb37
Crossroadsman <crossroadsman@gmail.com>
2018-10-03 07:47:05 -0600

    Add simple demo

---
+++ fetch/scotch_io/demo/app.js
@@ -0,0 +1,44 @@
+const ul = document.querySelector('#authors');
+const url = 'https://randomuser.me/api/?results=10';
+
+const createNode = (element) => {
+    return document.createElement(element);
+}
+
+const append = (parent, element) => {
+    return parent.appendChild(element)
+}
+
+const authorToLi = (author) => {
+    let li = createNode('li');
+    let img = createNode('img');
+    let span = createNode('span');
+
+    img.src = author.picture.medium;
+    span.innerHTML = `${author.name.first} ${author.name.last}`; // template literal: JS's equivalent to string interpolation
+    append(li, img);
+    append(li, span);
+    return li;
+}
+
+const jsonify = (fetch_response) => {
+    return fetch_response.json();
+}
+
+const response = (json_data) => {
+    let authors = json_data.results;
+    authors.map( (author) => {
+        const li = authorToLi(author);
+        append(ul, li);
+    });
+}
+
+const err = (data) => {
+    // handle the error case from the fetch
+    console.log(data);
+}
+
+fetch(url)
+.then(jsonify)
+.then(response)
+.catch(err);
