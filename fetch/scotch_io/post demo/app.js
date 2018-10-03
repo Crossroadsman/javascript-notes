f1064134e4850265516db8747dc37fb3a8fa1ac4
Crossroadsman <crossroadsman@gmail.com>
2018-10-03 09:32:54 -0600

    Add CORS header for fetch POST demo

--- fetch/scotch_io/post demo/app.js
+++ fetch/scotch_io/post demo/app.js
@@ -2,19 +2,33 @@ const url = 'https://randomuser.me/api';
 
 // request data for putting in the body of the POST request
 const data = {
-    name: 'Jake'
+    name: 'Sara'
 }
 
 // the parameters for creating the POST request
 // for Headers see https://developer.mozilla.org/en-US/docs/Web/API/Headers
 const fetchData = {
     method: 'POST',
-    body: data,
-    headers: new Headers()
+    body: JSON.stringify(data),
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
 }
 
 const jsonify = (fetchResponse) => {
-    return fetchResponse.json();
+    if (fetchResponse.ok) {
+        return fetchResponse.json();
+    } else {
+        // can either throw Error or reject the Promise
+        console.log('server responded with an error code');
+        rejection_data = {
+            status: fetchResponse.status,
+            statusText: fetchResponse.statusText
+        }
+        return Promise.reject(rejection_data);
+    }
+    
 }
 
 const addToPage = (jsonData) => {
@@ -23,7 +37,11 @@ const addToPage = (jsonData) => {
 }
 
 const fetchError = (err) => {
-    console.log(err);
+    if (err.status === 404) {
+        console.log("Server returned 404: Not Found");
+    } else {
+        console.log(err);
+    }
 }
 
 console.log('making fetch request');
